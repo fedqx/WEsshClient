51904c31312e8bb66cb26ff4cf0a5068152c45e9
Fatih Melih Özgün <ozgunfatihmelih@gmail.com>
2020-10-09 12:02:35 +0300

    put on hold because of a ssh issue

--- WEsshClient/Form1.Designer.cs
+++ WEsshClient/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.usernamebox = new System.Windows.Forms.TextBox();
             this.ipbox = new System.Windows.Forms.TextBox();
+            this.portbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.connectbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -66,11 +74,89 @@
             this.ipbox.Size = new System.Drawing.Size(100, 22);
             this.ipbox.TabIndex = 3;
             // 
+            // portbox
+            // 
+            this.portbox.Location = new System.Drawing.Point(192, 116);
+            this.portbox.Name = "portbox";
+            this.portbox.Size = new System.Drawing.Size(100, 22);
+            this.portbox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Port";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(66, 173);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Port Forwarding?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(67, 269);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Remote Port To Forward:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(237, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // connectbutton
+            // 
+            this.connectbutton.Location = new System.Drawing.Point(637, 34);
+            this.connectbutton.Name = "connectbutton";
+            this.connectbutton.Size = new System.Drawing.Size(117, 66);
+            this.connectbutton.TabIndex = 12;
+            this.connectbutton.Text = "Connect";
+            this.connectbutton.UseVisualStyleBackColor = true;
+            this.connectbutton.Click += new System.EventHandler(this.connectbutton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connectbutton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.portbox);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.ipbox);
             this.Controls.Add(this.usernamebox);
             this.Controls.Add(this.label2);
@@ -88,6 +174,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox usernamebox;
         private System.Windows.Forms.TextBox ipbox;
+        private System.Windows.Forms.TextBox portbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button connectbutton;
     }
 }
 
--- WEsshClient/Form1.cs
+++ WEsshClient/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,17 @@ namespace WEsshClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void connectbutton_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = "C:\\Windows\\System32\\cmd.exe";
+            process.StartInfo.Arguments = "/K C:\\Users\\f3dqx\\server.bat";
+            
+            process.Start();
+
         }
     }
 }
