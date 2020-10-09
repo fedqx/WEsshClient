25461db3cd29d54de8c4fb3994f6ee83456d9c37
Fatih Melih Özgün <ozgunfatihmelih@gmail.com>
2020-10-09 08:03:08 +0300

    started ui

--- WEsshClient/Form1.Designer.cs
+++ WEsshClient/Form1.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernamebox = new System.Windows.Forms.TextBox();
+            this.ipbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hostname or IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // usernamebox
+            // 
+            this.usernamebox.Location = new System.Drawing.Point(192, 74);
+            this.usernamebox.Name = "usernamebox";
+            this.usernamebox.Size = new System.Drawing.Size(100, 22);
+            this.usernamebox.TabIndex = 2;
+            // 
+            // ipbox
+            // 
+            this.ipbox.Location = new System.Drawing.Point(192, 29);
+            this.ipbox.Name = "ipbox";
+            this.ipbox.Size = new System.Drawing.Size(100, 22);
+            this.ipbox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ipbox);
+            this.Controls.Add(this.usernamebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernamebox;
+        private System.Windows.Forms.TextBox ipbox;
     }
 }
 
